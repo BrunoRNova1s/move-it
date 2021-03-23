d87cf411dcc801a023304b4bb15d79682d824570
Bruno Novais <NB25372@novabase.pt>
2021-03-23 01:11:00 +0000

    profile component

---
+++ web/moveit-next/src/components/Profile.tsx
@@ -0,0 +1,19 @@
+import styles from "../styles/pages/Profile.module.css";
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img
+        src="https://avatars.githubusercontent.com/u/11559401?s=400&u=d57149fdb4d77d0e60bc79017a784ef9dc382fe1&v=4"
+        alt="Bruno"
+      />
+      <div>
+        <strong>Bruno Novais</strong>
+        <p>
+          <img src="icons/level.svg" alt="level" />
+          Level 1
+        </p>
+      </div>
+    </div>
+  );
+}
